9586f3d9f50980e5850f00cf2c9c17a6dec90077
rwscherrer <rwscherrer@gmail.com>
2016-09-01 21:18:48 -0500

    class updates

--- app/controllers/accents_controller.rb
+++ app/controllers/accents_controller.rb
@@ -1,5 +1,47 @@
 class AccentsController < ApplicationController
 
+  def index
+    @all_accents = Accent.all
+
+  end
+
+  def show
+    @accent = Accent.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def new1
+
+  end
+
+
+  def create
+    @accent = Accent.create(name: params[:name],
+                            price: params[:price],
+                            image: params[:image],
+                            description: params[:description])
+    render 'show.html.erb'
+
+  end
+
+  def edit 
+    @accent = Accent.find(params[:id])
+  end
+
+  def update
+    @accent = Accent.find(params[:id])
+    @accent = Accent.update(name: params[:name], 
+                              price: params[:price], 
+                              image: params[:image], 
+                              description: params[:description])
+    render 'show.html.erb'
+  end
+    
+
+
   def all_accents
     @all_accents = Accent.all
 
@@ -17,5 +59,6 @@ class AccentsController < ApplicationController
   def send_form
     @email = params[:email]
   end
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get '/' => 'accents#index'
+  get '/accents' => 'accents#index'
+
+  get '/accents/new' => 'accents#new'
+  get '/accents/new1' => 'accents#new1'
+  post '/accents' => 'accents#create'
+  
+  get '/accents/:id' => 'accents#show'
+
+  get '/accents/:id/edit' => 'accents#edit'
+  patch '/accents/:id' => 'accents#update'
+  
+
   get '/one_accent' => 'accents#one_accent'
   get '/all_accents' => 'accents#all_accents'
   get '/get_form' => 'accents#get_form'
